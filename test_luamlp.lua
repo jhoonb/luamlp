2aaf99a5b89bd0b958f8b4ca6e67d63f2a713348
Jhonathan Paulo Banczek <jpbanczek@gmail.com>
2013-10-09 15:25:49 -0300

    Upload Source Code

---
+++ test_luamlp.lua
@@ -0,0 +1,18 @@
+--[[
+-- test module luamlp.lua
+-- Author: Jhonathan Paulo Banczek
+-- Copyright: jpbanczek@gmail.com
+-- 09-10-2013
+--]]
+
+luamlp = require 'luamlp'
+
+mlp = luamlp:New(2,2,0,1)
+
+-- XOR PROBLEM
+
+mlp.input = {{0,0},{0,1},{1,0},{1,1}}
+mlp.output = {{0},{1},{1},{0}}
+mlp:Config(0.3, 100000, 1, 0.01, 0.003)
+mlp:Training(true)
+mlp:Test(mlp.input, mlp.output)
